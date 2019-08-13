01eb0c7ec6a428fea187a06627c06f386e388aec
Guillaume Queste <guillaume.queste@laposte.net>
2019-08-13 10:57:01 +0200

    29

--- class/OpenWeather.php
+++ class/OpenWeather.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'APIException.php';
 class OpenWeather {
 
     private $apiKey;
@@ -9,7 +10,7 @@ class OpenWeather {
     }
 
     public function getForecast(string $city): ?array // ? : peut être null
-    {
+    {   
         $curl = curl_init("http://api.openweathermap.org/data/2.5/forecast?q={$city}&units=metric&lang=fr&APPID={$this->apiKey}");
         curl_setopt_array($curl, [
             CURLOPT_RETURNTRANSFER => true,
@@ -17,11 +18,22 @@ class OpenWeather {
             CURLOPT_TIMEOUT => 1
         ]);
         $data = curl_exec($curl);
-        if ($data === false || curl_getinfo($curl, CURLINFO_HTTP_CODE) !== 200) {
-            return null;
+        // si l'exécution de curl n'a pas fonctionné, on récupère l'erreur
+        if ($data === false) {
+            $error = curl_error($curl);
+            curl_close($curl);
+            // j'envoie une exception avec la classe APIException que j'ai créée et qui hérite de la classe Exception
+            throw new APIException($error);
+        }
+        // si le serveur ne répond pas avec un statut 200 (succès de la requête), on renvoie les données récupérées
+        if (curl_getinfo($curl, CURLINFO_HTTP_CODE) !== 200) {
+            // j'envoie une exception avec la classe Exception
+            throw new Exception($data);
         }
         $results = [];
         $data = json_decode($data, true); // true car je souhaite avoir un tableau associatif
+        curl_close($curl);
+        
         foreach ($data['list'] as $threeHour) {
             $results[] = [
                 'temp' => $threeHour['main']['temp'],
--- meteo.php
+++ meteo.php
@@ -1,10 +1,29 @@
 <?php
 require_once 'class/OpenWeather.php';
-$weather = new OpenWeather('51fc8505d55fe845e4c757ea6b9ff0cf');
-$forecast = $weather->getForecast('Dieppe,fr');
+$error = null;
+try { // j'essaie d'exécuter la requête
+    $weather = new OpenWeather('51fc8505d55fe845e4c757ea6b9ff0cf');
+    $forecast = $weather->getForecast('Dieppe,fr');
+} catch (Exception $e) {
+    $error = $e->getMessage();
+} // finally {...} : le code à exécuter quoi qu'il arrive
+
+/* catch (APIException $e) { // si je n'arrive pas à exécuter la requête, j'attrape le message d'erreur en utilisant la classe APIException que j'ai créée
+    $error = 'Erreur API';
+    // $error = $e->getMessage();
+} catch (Exception $e) { // si je n'arrive pas à exécuter la requête, j'attrape le message d'erreur
+    $error = 'Erreur classique';
+    // $error = $e->getMessage();
+} */
+
 require 'header.php';
 ?>
 
+<!-- si j'ai une erreur, je l'affiche -->
+<?php if ($error): ?>
+<div class="alert alert-danger"><?= $error ?></div>
+<!-- sinon j'affiche les données -->
+<?php else: ?>
 <div class="container">
     <p>Le <?= $forecast[0]['date']->format('d/m/Y') ?> : <?= $forecast[0]['temp'] ?> °C - <?= $forecast[0]['description'] ?></p>
     <ul>
@@ -13,5 +32,6 @@ require 'header.php';
         <?php endforeach ?>
     </ul>
 </div>
+<?php endif ?>
 
 <?php require 'footer.php'; ?>
